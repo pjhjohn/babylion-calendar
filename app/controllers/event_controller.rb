eab7da4a2a806722a1022466201cfddc78fed523
JoonHo Park <pjhjohn@gmail.com>
2015-08-29 07:41:39 +0000

    Added Basic features including bootstrap-calendar

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,25 @@
+class EventController < ApplicationController
+  def create
+  end
+
+  def read
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def remove
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root :to => "main#index"
+  match ":controller(/:action(/:id))", :via => [:get, :post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
